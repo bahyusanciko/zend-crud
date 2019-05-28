374b93f63ed3c914f706eae383ed7d95c9710872
Bahyu Sanciko <41245088+bahyusanciko@users.noreply.github.com>
2019-05-28 15:40:43 +0700

    Update MenuController.php

--- application/controllers/MenuController.php
+++ application/controllers/MenuController.php
@@ -10,33 +10,20 @@ class MenuController extends Zend_Controller_Action{
         $this->session = new Zend_Session_Namespace(self::SESSION_NAMESPACE);
     }
 
-    public function indexAction()
-    {
+    public function indexAction(){
         $this->view->user =  $this->session->user['nama_admin'];
     }
     public function departmentAction($value=''){
         
     }
-    public function jabatanAction($value='')
-    {
+    public function jabatanAction($value=''){
         
     }
-    public function adminAction($value='')
-    {
+    public function adminAction($value=''){
         # code...
     }
     public function reportAction($value=''){
-        $data = $this->model->allJab();
-        foreach ($data as $row ) {
-            $itemArray[] = 
-            array(
-                 $row['kd_jabatan'],
-                 $row['nama_department'],
-                 $row['nama_jabatan'],
-                 $row['desc_department']
-                 );
-        }
-        $this->view->data = json_encode($itemArray);
+        
     }
 }
 
